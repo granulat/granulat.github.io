4555335e9c2c9ed07879597e21aef33914a2bdb9
matto <matto@damniam.de>
2019-02-22 22:56:22 +0100

    woof woof

--- js/template.js
+++ js/template.js
@@ -3,7 +3,7 @@
 var template = `
 <div class="card">
     <h2>{{name}}</h2>
-    <img src="https://thispersondoesnotexist.com/?{{name}}" />
+    <img src="https://thispersondoesnotexist.com/image?{{name}}" />
     <div>{{info}}</div>
 </div>
 `
